48359b36b693a747fe6db0888667757076d4096e
fukushima-c <fukushima.czn@gmail.com>
2019-07-29 16:58:56 +0900

    コメント変更

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,13 @@
 //　通信ダミー
 var serif = new Array();
-serif[1] = "こんにちは";
-serif[2] = "おはよう";
-serif[3] = "いらっしゃいませ";
-serif[4] = "御用でしょうか";
-serif[5] = "寒いですか";
-serif[6] = "それでは";
+serif[1] = "こんにちは！";
+serif[2] = "おはよっ！";
+serif[3] = "たのしい？";
+serif[4] = "おなかへってる？";
+serif[5] = "暑くない？";
+serif[6] = "じゃあねー";
 
-serif[7] = "タップされました";
+serif[7] = "タップしたよ";
 
 //ランダムにダミー通信を表示
 function msg() {
@@ -15,12 +15,57 @@ function msg() {
 	$(".fukidashi").text(serif[a + 1]);
 }
 
-//プッシュ通信のメッセージを吹き出しに表示
-function msg02(message) {
-	$(".fukidashi").text(message);
+function execute() {
+  var username = "user1";
+  var password = "123ABC";
+  KiiUser.authenticate(username, password).then(
+    function(theUser) {
+      var development = false;
+      return KiiUser.getCurrentUser().pushInstallation().installMqtt(development);
+    }
+  ).then(
+    function(response) {
+      var installationID = response.installationID;
+      return KiiUser.getCurrentUser().pushInstallation().getMqttEndpoint(installationID);
+    }
+  ).then(
+    function(response) {
+      var username = response.username;
+      var password = response.password;
+      var mqttTopic = response.mqttTopic;
+
+      var endpoint = "wss://" + response.host + ":" + response.portWSS + "/mqtt";
+      var client = mqtt.connect(endpoint, {
+        username: username,
+        password: password,
+        clientId: mqttTopic,
+      });
+
+      client.on("connect", function() {
+        console.log("MQTT Connected");
+        client.subscribe(mqttTopic, function() {
+          console.log("MQTT Subscribed");
+        });
+      });
+
+      client.on("message", function(topic, message, packet) {
+        if (topic === mqttTopic) {
+          var payload = JSON.parse(message.toString());
+          var myMessage = payload.mymessage;
+          alert("Message Arrived:" + myMessage);
+        }
+      });
+
+      client.on("error", function(error) {
+        alert("Error in MQTT" + error);
+      });
+    }
+  ).catch(
+    function(error) {
+      var errorString = error.message;
+      alert("Error in Initialization: " + errorString);
+    }
+  );
+
 }
 
-	//プッシュ通信受信承認
-Notification.requestPermission(function(status) {
-	console.log('通知の許可：', status);
-});
