86dd90c11be135276112977ef034a9cca120e369
fukushima-c <fukushima.czn@gmail.com>
2019-07-30 18:02:31 +0900

    ユーザ作成

--- scripts/main.js
+++ scripts/main.js
@@ -14,62 +14,19 @@ function msg() {
 	$(".fukidashi").text(serif[a + 1]);
 }
 
-
 function execute() {
-  var username = "user1";
-  var password = "123ABC";
-  KiiUser.authenticate(username, password).then(
-    function(theUser) {
-      var development = false;
-      return KiiUser.getCurrentUser().pushInstallation().installMqtt(development);
-    }
-  ).then(
-    function(response) {
-      var installationID = response.installationID;
-      return KiiUser.getCurrentUser().pushInstallation().getMqttEndpoint(installationID);
-    }
-  ).then(
-    function(response) {
-      var username = response.username;
-      var password = response.password;
-      var mqttTopic = response.mqttTopic;
-
-      var endpoint = "wss://" + response.host + ":" + response.portWSS + "/mqtt";
-      var clientId = mqttTopic;
-      var client = new Paho.MQTT.Client(endpoint, clientId);
-
-      client.onConnectionLost = onConnectionLost;
-      client.onMessageArrived = onMessageArrived;
-
-      client.connect({onSuccess:onConnect,
-        userName:username,
-        password:password,
-      });
-
-      function onConnect() {
-        console.log("MQTT Connected");
-        client.subscribe(mqttTopic);
-      }
-
-      function onConnectionLost(responseObject) {
-        if (responseObject.errorCode !== 0) {
-            alert("MQTT Connection Lost:" + responseObject.errorMessage);
-        }
-      }
-
-      function onMessageArrived(message) {
-        if (message.destinationName === mqttTopic) {
-          var payload = JSON.parse(message.payloadString);
-          var myMessage = payload.mymessage;
-          alert("Message Arrived:" + myMessage);
-        }
-      }
-    }
-  ).catch(
-    function(error) {
-      var errorString = error.message;
-      alert("Error in Initialization: " + errorString);
-    }
-  );
+	var username = "user1";
+	var password = "123ABC";
+	var user = KiiUser.userWithUsername(username, password);
+	user.register().then(
+		function(theUser) {
+			alert("Succeeded");
+		}
+	).catch(
+		function(error) {
+			var errorString = error.message;
+			alert("Error: " + errorString);
+		}
+	);
 }
 
