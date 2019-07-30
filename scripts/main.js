6a3de59792e89f8aa1334f6b25244571aa22c098
fukushima-c <fukushima.czn@gmail.com>
2019-07-30 12:51:04 +0900

    ユーザー作成

--- scripts/main.js
+++ scripts/main.js
@@ -14,56 +14,18 @@ function msg() {
 	$(".fukidashi").text(serif[a + 1]);
 }
 
-// 
 function execute() {
   var username = "user1";
   var password = "123ABC";
-  KiiUser.authenticate(username, password).then(
+  var user = KiiUser.userWithUsername(username, password);
+  user.register().then(
     function(theUser) {
-      var development = false;
-      return KiiUser.getCurrentUser().pushInstallation().installMqtt(development);
-    }
-  ).then(
-    function(response) {
-      var installationID = response.installationID;
-      return KiiUser.getCurrentUser().pushInstallation().getMqttEndpoint(installationID);
-    }
-  ).then(
-    function(response) {
-      var username = response.username;
-      var password = response.password;
-      var mqttTopic = response.mqttTopic;
-
-      var endpoint = "wss://" + response.host + ":" + response.portWSS + "/mqtt";
-      var client = mqtt.connect(endpoint, {
-        username: username,
-        password: password,
-        clientId: mqttTopic,
-      });
-
-      client.on("connect", function() {
-        console.log("MQTT Connected");
-        client.subscribe(mqttTopic, function() {
-          console.log("MQTT Subscribed");
-        });
-      });
-
-      client.on("message", function(topic, message, packet) {
-        if (topic === mqttTopic) {
-          var payload = JSON.parse(message.toString());
-          var myMessage = payload.mymessage;
-          alert("Message Arrived:" + myMessage);
-        }
-      });
-
-      client.on("error", function(error) {
-        alert("Error in MQTT" + error);
-      });
+      alert("Succeeded");
     }
   ).catch(
     function(error) {
       var errorString = error.message;
-      alert("Error in Initialization: " + errorString);
+      alert("Error: " + errorString);
     }
   );
 }
